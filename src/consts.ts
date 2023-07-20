bb21e362900344b69e14e1de5399389297d00e0d
Steve Taylor Witte <stevetwitte@gmail.com>
2023-07-20 18:22:53 -0400

    Start work on new Astro site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'steve taylor witte';
+export const SITE_DESCRIPTION = 'online home of a software engineer, musician, creative';
